b7998c721de57165dae2b83418afc275fb5cb14e
usman <usman.solemn@gmail.com>
2020-01-19 15:33:46 +0500

    init, Login using github in Asp.net core mvc

---
+++ DotNetCoreOAuth/Web/AuthorizeWithGithub/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AuthorizeWithGithub.Models;
+using System.Security.Claims;
+
+namespace AuthorizeWithGithub.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new GithubModel();
+            if (User.Identity.IsAuthenticated)
+            {
+                model.GitHubName = User.FindFirst(c => c.Type == ClaimTypes.Name)?.Value;
+                model.GitHubLogin = User.FindFirst(c => c.Type == "urn:github:login")?.Value;
+                model.GitHubUrl = User.FindFirst(c => c.Type == "urn:github:url")?.Value;
+                model.GitHubAvatar = User.FindFirst(c => c.Type == "urn:github:avatar")?.Value;
+            }
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DotNetCoreOAuth/Web/AuthorizeWithGithub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthorizeWithGithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
